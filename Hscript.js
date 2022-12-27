3e4a53faf31882feca832681ef450703dc91e915
Olesya Figurovskaya <lisonkadrug@gmail.com>
2022-12-27 10:20:06 +0300

    plugin added

--- Hscript.js
+++ Hscript.js
@@ -1,14 +1,21 @@
-let slides = document.querySelectorAll('.slide');
 
-slides.forEach(function(slide) {
-    slide.addEventListener('click', function() {
-        clearActiveSlide();
-        slide.classList.add('active');
-    })
-});
+function slidesPlugin(activeSlide = 0) {
+    let slides = document.querySelectorAll('.slide');
 
-function clearActiveSlide() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    })
+    slides[activeSlide].classList.add('active');
+    
+    slides.forEach(function(slide) {
+        slide.addEventListener('click', function() {
+            clearActiveSlide();
+            slide.classList.add('active');
+        })
+    });
+    
+    function clearActiveSlide() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
 }
+
+slidesPlugin();
